f132ee6fe704d35c6dc96f67f4b2c71c2ca94ad6
Jessy Schlahmilch <jessyschlahmilch@hotmail.com>
2022-11-29 16:22:52 +0100

    fix: split code and dynamic content

---
+++ components/Agenda/AgendaView.vue
@@ -0,0 +1,13 @@
+<template >
+    <div class="flex flex-col space-y-3">
+        <div>
+            <h1 class="text-3xl font-bold">Aankomende afspraken</h1>
+        </div>
+        <AgendaAppointment date="12 Mei, 2023" time="10:00" name="Dr. Van Der velden" department="Psychologie"
+            steps="0/3 stappen voltooid" />
+        <AgendaAppointment date="24 Juni, 2023" time="15:00" name="Dr. Spoellemann" department="Praktijk ondersteuner"
+            steps="0/3 stappen voltooid" />
+
+    </div>
+
+</template>
