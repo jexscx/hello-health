3d9556a2cbb0506faaac7267cec1969b41312697
Jessy-Schlahmilch <jessyschlahmilch@hotmail.com>
2022-11-30 16:08:00 +0100

    add: state management

--- components/Question/QuestionButton.vue
+++ components/Question/QuestionButton.vue
@@ -1,7 +1,12 @@
 <template>
     <div class="flex flex-col gap-2 mt-4">
-        <div v-for="question of questions" :key="question">
-            <button class="outline outline-1 outline-orange-300 rounded w-full h-12">
+        <div v-for="(question, i) of questions" :key="question">
+            <button :id="i.toString()" v-if="isActive" @click="active()"
+                class="outline outline-1  bg-orange-300 outline-orange-300 rounded w-full h-12">
+                <p class="text-left font-bold text-white pl-4">{{ question }}</p>
+            </button>
+            <button @click="active()" :id="i.toString()" v-else
+                class="outline outline-1 outline-orange-300 rounded w-full h-12">
                 <p class="text-left pl-4">{{ question }}</p>
             </button>
         </div>
@@ -11,7 +16,12 @@
 <script setup lang="ts">
 interface Props {
     questions: string[];
+
 }
 
 defineProps<Props>();
+const isActive = useState('isActive', () => false);
+const active = () => {
+    isActive.value = !isActive.value;
+}
 </script>
--- components/Question/QuestionPrefix.vue
+++ components/Question/QuestionPrefix.vue
@@ -1,20 +1,25 @@
 <template>
-    <div class="flex flex-col">
+    <div class="flex flex-col max-w-md">
         <div>
-            <h1 class="text-3xl font-bold">Goede vragen om te stellen</h1>
+            <h1 class="text-3xl font-bold">Suggestie vragen om te stellen</h1>
         </div>
         <div class="grid grid-cols-2 items-center">
             <h6 class="text-xl font-bold">Dr. Velden V.d. Linden</h6>
             <p class="text-base text-right tracking-wide">Psychologie</p>
         </div>
-        <div class="w-1">
-            <!-- <p>Hier staan een paar vragen die je kunt stellen.
-                deze vragen zijn er om je op weg te helpen.
-                Klik op om de vraag toe te voegen
-                aan je vragenlijst.</p> -->
+        <div class="flex flex-col">
+            <p>Hier staan een paar vragen die je kunt stellen.</p>
+            <p>Deze vragen zijn er om je op weg te helpen.</p>
+            <p>Klik op de vraag om deze toe te voegen aan de lijst</p>
         </div>
-        <QuestionButton
-            :questions="['Uit welke behandelingen kan ik kiezen?', 'Welke behandeling raadt u aan?', 'Waarom wordt deze behandeling gekozen?', 'Mag ik wat bedenktijd?']">
+        <QuestionButton :questions="questions">
         </QuestionButton>
     </div>
 </template>
+
+<script setup>
+const questions = [
+    'Uit welke behandelingen kan ik kiezen?', 'Welke behandeling raadt u aan?', 'Waarom wordt deze behandeling gekozen?', 'Mag ik wat bedenktijd?'
+
+]
+</script>
