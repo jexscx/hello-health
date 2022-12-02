822c782ade78c163774688cfdf2101076a0b971a
Jessy Schlahmilch <jessyschlahmilch@hotmail.com>
2022-12-02 14:37:32 +0100

    Fix: Styling

--- components/Question/QuestionItem.vue
+++ components/Question/QuestionItem.vue
@@ -1,15 +1,6 @@
 <template>
   <div
-    class="
-      outline outline-2 outline-orange-300
-      rounded
-      w-full
-      h-12
-      text-left
-      pl-4
-      flex
-      items-center
-    "
+    class="outline outline-2 outline-orange-300 rounded w-full h-12 text-left pl-4 flex items-center"
     :class="{ 'bg-orange-300 text-white font-bold': selected }"
     @click="$emit('click', $event)"
   >
