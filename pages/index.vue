54422e37be617ffc6a1b78e9842f2a033ed8b034
Jessy Schlahmilch <jessyschlahmilch@hotmail.com>
2022-12-02 14:37:53 +0100

    add: Dynamic data

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,24 @@
 <template>
-    <div class="flex flex-col space-y-3">
-        <div>
-            <h1 class="text-3xl font-bold">Aankomende afspraken</h1>
-        </div>
-        <CalendarItem date="12 Mei, 2023" time="10:00" name="Dr. Van Der velden" department="Psychologie"
-            steps="0/3 stappen voltooid" />
-        <CalendarItem date="24 Juni, 2023" time="15:00" name="Dr. Spoellemann" department="Praktijk ondersteuner"
-            steps="0/3 stappen voltooid" />
+  <div class="flex flex-col space-y-3" v-if="!pending">
+    <div>
+      <h1 class="text-3xl font-bold">Aankomende afspraken</h1>
     </div>
+    <CalendarItem
+      v-for="appointment of data?.appointments"
+      :slug="appointment.slug"
+      :date="convertDate(appointment.date)"
+      :name="appointment.person"
+      :department="appointment.department"
+    />
+  </div>
+  <span v-else>Loading...</span>
 </template>
+
+<script setup lang="ts">
+import { Appointment } from "~~/utils/models";
+
+interface ApiResult {
+  appointments: Appointment[];
+}
+const { data, pending } = await useFetch<ApiResult>("/api/appointments");
+</script>
