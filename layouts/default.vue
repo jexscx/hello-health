a00fafae911a03c72a746cd95866a5a0dab38334
Jessy Schlahmilch <jessyschlahmilch@hotmail.com>
2022-12-06 19:47:10 +0100

    Change: layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,9 @@
 <template>
-  <div class="grid grid-cols-[1fr_4fr] h-full w-full gap-4">
-    <DesktopMenu />
-    <div class="shadow-lg grid justify-center pt-32">
+  <div class="grid grid-cols-[1fr_4fr] h-full w-full gap-4 max-md:grid-cols-1">
+    <Menu class="max-md:hidden" />
+    <div
+      class="shadow-lg grid lg:px-32 lg:pt-32 md:pt-12 md:px-12 sm:pt-5 sm:px-5"
+    >
       <slot></slot>
     </div>
   </div>
