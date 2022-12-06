54448272ee47bbf0ba6b92601e491b56f01feac3
jexscx <jessyschlahmilch@hotmail.com>
2022-12-06 12:45:35 +0100

    Add: nav static data

--- components/Desktop/DesktopMenu.vue
+++ components/Desktop/DesktopMenu.vue
@@ -1,14 +1,30 @@
 <template>
-  <div class="flex flex-col justify-center">
-    <div>
-      <NuxtLink to="/">
-        <h1 class="lg:text-center font-bold">Agenda</h1>
+  <div class="flex flex-col gap-4 pl-4 pt-12 rounded-md">
+    <NuxtLink to="/" class="flex h-12 flex-row items-center pl-4">
+      <svg
+        class="w-6 h-6 fill-none stroke-sky-900 stroke-2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      >
+        <use href="/feather-sprite.svg#home" />
+      </svg>
+      <h1 class="lg:text-center pl-2 font-bold content-center text-sky-900">
+        Startpagina
+      </h1>
+    </NuxtLink>
+    <div class="flex h-12 bg-sky-900/20 rounded-md">
+      <NuxtLink to="/" class="flex flex-row items-center pl-4">
+        <svg
+          class="w-6 h-6 fill-none stroke-sky-900 stroke-2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        >
+          <use href="/feather-sprite.svg#calendar" />
+        </svg>
+        <h1 class="lg:text-center pl-2 font-bold content-center text-sky-900">
+          Agenda
+        </h1>
       </NuxtLink>
-    </div>
-    <div>
-      <!-- <NuxtLink to="/">
-        <h1 class="lg:text-center font-bold">Geschiedenis</h1>
-      </NuxtLink> -->
     </div>
   </div>
 </template>
