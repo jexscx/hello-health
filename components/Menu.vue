7ef5502bc8e6be1be52ae139582e5bb832329e3f
Jessy Schlahmilch <jessyschlahmilch@hotmail.com>
2022-12-07 14:32:46 +0100

    Add: clear local storage for testing purposes

--- components/Menu.vue
+++ components/Menu.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="flex flex-col gap-4 pl-4 pt-12 rounded-md">
-    <img src="/img/logo@2x.png" />
+    <div class="w-3/4 pl-4">
+      <img src="/img/logo@2x.png" />
+    </div>
     <NuxtLink to="/" class="flex h-12 flex-row items-center pl-4">
       <svg
         class="w-6 h-6 fill-none stroke-sky-900 stroke-2"
@@ -13,19 +15,21 @@
         Startpagina
       </h1>
     </NuxtLink>
-    <div class="flex h-12 bg-sky-900/20 rounded-md">
-      <NuxtLink to="/" class="flex flex-row items-center pl-4">
-        <svg
-          class="w-6 h-6 fill-none stroke-sky-900 stroke-2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-        >
-          <use href="/feather-sprite.svg#calendar" />
-        </svg>
-        <h1 class="lg:text-center pl-2 font-bold content-center text-sky-900">
-          Agenda
-        </h1>
-      </NuxtLink>
-    </div>
+    <NuxtLink
+      to="/"
+      class="flex flex-row items-center pl-4 h-12 bg-sky-900/20 rounded-md"
+    >
+      <svg
+        class="w-6 h-6 fill-none stroke-sky-900 stroke-2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      >
+        <use href="/feather-sprite.svg#calendar" />
+      </svg>
+      <h1 class="lg:text-center pl-2 font-bold content-center text-sky-900">
+        Agenda
+      </h1>
+    </NuxtLink>
+    <StorageClear />
   </div>
 </template>
---
+++ components/StorageClear.vue
@@ -0,0 +1,11 @@
+<template>
+  <button class="outline outline-slate-100 h-7" @click="resetLocalStorage()">
+    <span class="text-slate-100">Reset test</span>
+  </button>
+</template>
+
+<script setup lang="ts">
+function resetLocalStorage() {
+  localStorage.clear();
+}
+</script>
