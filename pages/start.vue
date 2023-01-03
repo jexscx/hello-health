dc6aebd446f5a87c080422330e187a571181939b
Jessy Schlahmilch <jessyschlahmilch@hotmail.com>
2023-01-03 17:00:33 +0100

    added startpagina component

---
+++ pages/start.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="flex flex-col gap-4">
+    Dit is de startpagina, die doet nu nog niets!
+
+    <NuxtLink class="bg-purple-900 text-white p-4 text-bold" to="/">
+      Ga naar de agenda pagina
+    </NuxtLink>
+  </div>
+</template>
